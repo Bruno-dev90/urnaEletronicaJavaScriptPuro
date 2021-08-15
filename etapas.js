123e70b03d26b89886857fd149e2c93ccbccd1c3
Bruno Antonio <brun0am0reira@hotmail.com>
2021-08-15 13:27:29 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        titulo:"VEREADOR", 
+        numeros: 5,
+        candidatos:[
+            {
+                numero:'38111',
+                nome:"Fulano de Tal",
+                partido:"ABC",
+                fotos:[
+                    {url:'Wander.jpg', legenda: "Vereador", smal:false}    
+                ]
+            },
+            {
+                numero:'77222', 
+                nome: 'Beltrano da Silva',
+                partido: "DEFG",
+                fotos:[
+                    {url:'Terezinha.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero:'20',
+                nome: 'Cliclano',
+                partido:'ABC',
+                vice:'Cic',
+                fotos:[
+                    {url:'Rodolfo.jpeg', legenda: "Prefeito"},
+                    {url:'Ricardinho.jfif', legenda:"Vice-Prefeito", smal:true }
+                ]
+            },
+            {
+                numero:'84',
+                nome:'Zulano',
+                partido:'QWERTY',
+                vice:'Zul',
+                fotos:[
+                    {url:'Wander.jpg', legenda:'Prefeito'},
+                    {url:'Terezinha.jpg', legenda:'Vice-Prefeito', smal:true}
+                ]
+            }
+        ]
+
+    }
+
+
+];
